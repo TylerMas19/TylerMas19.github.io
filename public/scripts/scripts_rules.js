034df2b87161d9faf74fdd84fe0847c14b58be1e
TylerMas19 <tyler@islandmusings.com>
2018-11-30 09:38:18 -0500

    all

--- public/scripts/scripts_rules.js
+++ public/scripts/scripts_rules.js
@@ -2,5 +2,5 @@ document.getElementById("show_stats_button").addEventListener("click",function()
   location.href = "/stats";
 });
 document.getElementById("back_to_game_button").addEventListener("click",function(){
-  location.href = "";
+  location.href = "/";
 });
